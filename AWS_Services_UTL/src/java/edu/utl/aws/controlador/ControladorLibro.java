15a8b3298a255ffb7e670581b7cc7dac40336ac0
Cenpasuchitl <flor.navac.05@gmail.com>
2025-06-18 21:26:47 -0600

    Proyecto completo

--- AWS_Services_UTL/src/java/edu/utl/aws/controlador/ControladorLibro.java
+++ AWS_Services_UTL/src/java/edu/utl/aws/controlador/ControladorLibro.java
@@ -5,24 +5,29 @@ import edu.utl.aws.conexionBD.ConexionSQL;
 import java.sql.*;
 
 public class ControladorLibro{
-    public void insertarLibro (Libro l) throws SQLException{
-        String query = "";
-        
-        try {
-            ConexionSQL connMysql = new ConexionSQL();
-            Connection conn = connMysql.open();
-            PreparedStatement pstm = conn.prepareStatement(query);
-            
-            pstm.setInt(1, l.getId());
-            pstm.setString(2, l.getTitulo());
-            pstm.setInt(3, l.getEditorial().getId());
+    
+    public void insertarLibro(Libro l) throws SQLException {
+    // Consulta SQL corregida con AUTO_INCREMENT y nombres reales
+    String query = "INSERT INTO libros (titulo, editorial_id) VALUES (?, ?)";
 
-            pstm.execute();
-            System.out.println("Registro exitoso");
-            pstm.close();
-            connMysql.close();
-        }catch(SQLException e){
-            e.getStackTrace();
-        }
+    try {
+        ConexionSQL connMysql = new ConexionSQL();
+        Connection conn = connMysql.open();
+        PreparedStatement pstm = conn.prepareStatement(query);
+
+        // Solo seteamos título y editorial_id
+        pstm.setString(1, l.getTitulo());
+        pstm.setInt(2, l.getEditorial().getId());
+
+        pstm.executeUpdate(); // se recomienda usar executeUpdate() para INSERT
+        System.out.println("Registro exitoso");
+
+        pstm.close();
+        connMysql.close();
+    } catch(SQLException e) {
+        e.printStackTrace(); // Asegura que el error se imprima en consola
+        throw e; // También lanza la excepción para que RESTLibro pueda manejarla
     }
+}
+
 }
